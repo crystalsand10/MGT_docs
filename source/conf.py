7171795670b6f6e15bc389388345c3a4599f597d
Sand <jarvis@Sands-MacBook-Pro.local>
2023-10-20 15:51:46 +1100

    fixing theme settings

--- source/conf.py
+++ source/conf.py
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -175,9 +175,9 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
-extensions = [
-    'sphinx_rtd_theme',
-]
+# extensions = [
+#    'sphinx_rtd_theme',
+# ]
 
 # -- Options for todo extension ----------------------------------------------
 
